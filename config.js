a8003b5729e42ad67ee7148fc59dd05e37d7cec4
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-06-19 12:35:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAtk7rWxT7VFKA6J1UVHicHSakceMc4TA",
+    authDomain: "openclass-565ca.firebaseapp.com",
+    projectId: "openclass-565ca",
+    storageBucket: "openclass-565ca.firebasestorage.app",
+    messagingSenderId: "43042586871",
+    appId: "1:43042586871:web:d35842d6f67d44c24fd3f2",
+    measurementId: "G-BR2YDKX6B4"
+};
+
+export const emailConfig = {
+    serviceID: "service_djr1n0f",
+    templateID: "template_tiqnb53",
+    publicKey: "P88qoYcr5CHYVNAPw"
+};
+
+export const customAiConfig = {
+    // Replace 123.45.67.89 with your actual Hostinger VPS IP Address
+    backendUrl: "https://dance-peroxide-tinfoil.ngrok-free.dev/", 
+    projectKey: "my_super_secure_secret_agent_key_123",
+    groqKey: "" // Leave this empty, you don't need Groq anymore!
+};
